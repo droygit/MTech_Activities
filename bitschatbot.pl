a9cac98f0c48dd2fac45656c08db9abf9f586114
droygit <59766540+droygit@users.noreply.github.com>
2020-11-26 14:14:33 +0530

    Add files via upload
    Completed the implementation.

--- bitschatbot.pl
+++ bitschatbot.pl
@@ -3,22 +3,60 @@
 
 :- dynamic continue_menu/1.
 :- dynamic show_degree_info/1.
+:- dynamic app_status_command/2.
+
+
+read_string(OString):-
+    current_input(Input),
+    read_line_to_codes(Input, Codes),
+    string_codes(OString, Codes).
 
 % Initialization procedures
 start:-
-    reconsult('bitstemp.pl'),nl,
+    reconsult('bitsdb.pl'),nl,
     reconsult('bitsrules.pl'),nl,
-    reconsult('bitscourseinfo.pl'),nl,
+    reconsult('bitsinfo.pl'),nl,
     main_menu.
 
 % Saves new temporary info for use by inference engine
 save:-
     write('Saving data'),nl,
-    tell('bitstemp.pl'),
-    listing(grade),
-    listing(interest),
+    tell('bitsdb.pl'),
+    listing(bits_payment),
+    listing(bits_score),
     told,
-    write('Done'),nl.
+    write('Bye Bye!'),nl.
+
+app_status_command(BitsId,1):-
+    (bits_score(BitsId,OMarks) ->
+        nl,write("Congratulation! You got "), write(OMarks), write(" marks"),
+        nl,write("You are selected.");
+        nl,write("Sorry! You are not selected.")
+    ),
+    nl.
+
+app_status_command(BitsId,2):-
+    write("Please provide the payment reference number - "),nl,
+    read(PaymentRef),
+    ((bits_payment(BitsId,PaymentRef)) ->
+        nl,write("I confirm that your payment is done and registered with us.");
+        nl,write("Sorry! I couldn't find the BITSID and reference")
+    ),
+    nl.
+
+app_status_command(BitsId,3):-
+    nl,write("Please provide the payment reference number - "),nl,
+    read(PaymentRef),
+    nl,write("Please provide your BITS Score: "),
+    read(BitsScore),
+    (is_eligible(BitsScore) ->
+        assertz(bits_payment(BitsId,PaymentRef)),
+        assertz(bits_score(BitsId,BitsScore)),
+        nl,write("Your BITSID = "), write(BitsId), write(" and Payment Reference No.= "),write(PaymentRef),nl,
+        write("I will update the data base once you are done with your quiries.");
+        write("Sorry. You couldn't clear the cutoff marks. I can't register your payment information.")
+    ),
+    nl.
 
 command(1,IName):-
     show_all_courses(),
@@ -31,35 +69,54 @@ command(2,IName):-
     nl.
 
 command(3,IName):-
-    write("Not implemented yet."),nl,
+    info_application_procedure(),
     continue_menu(IName),
     nl.
 
 command(4,IName):-
-    write("Not implemented yet."),nl,
+    write("Hi "),
+    write(IName),
+    write(". Please provide your BITS id - "),nl,
+    read(Bitsid),
+    format('Just to confirm, your BITS ID = ~s',[Bitsid]),
+    nl,write("Please choose one of the following options - "),nl,
+    nl,write("1. Get your BITS Score"),
+    nl,write("2. If you already paid the amount"),
+    nl,write("3. If you paid the amount and want to register your BITS ID"),nl,
+    read(Option),
+    ((Option =:= 1 ; Option =:= 2 ; Option =:= 3) ->
+        app_status_command(Bitsid,1);
+        nl,write("Incorrect option. Please try again.")
+    ),
+    %((Option =:= 1) ->
+    %    app_status_command(Bitsid,1);
+    %    ((Option =:= 2) ->
+    %        app_status_command(Bitsid,2);
+    %        ((Option =:= 3) ->
+    %            app_status_command(Bitsid,3);
+    %            nl,write("Incorrect option. Please try again.")
+    %        )
+    %    )
+    %),
     continue_menu(IName),
     nl.
 
 command(5,IName):-
-    write("Not implemented yet."),nl,
+    info_admission_procedure(),
+    info_scholarship(),
+    info_bitsat_score_analysis(),
     continue_menu(IName),
     nl.
 
 command(6,IName):-
-    write("Not implemented yet."),nl,
     continue_menu(IName),
     nl.
 
 command(7,IName):-
-    write("Thanks for showing interest! Bye "),
+    write("Thanks for showing interest "),
     write(IName),
     write("! Talk to you later."),
     nl.
-
-read_name(OString):-
-    current_input(Input),
-    read_line_to_codes(Input, Codes),
-    string_codes(OString, Codes).
 
 general_options:-
     write("Choose one of the follwing number to proceed -"),nl,
@@ -73,22 +130,32 @@ general_options:-
 
 
 continue_menu(IName):-
+    nl,nl,
     write("Welcome back "),
     write(IName),
     write(" to main menu. Please select the next topic - "),nl,
     general_options(),
     read(Option),
-    command(Option,IName),
+    ((Option =:= 1 ; Option =:= 2 ; Option =:= 3 ; Option =:= 4 ; Option =:= 5 ; Option =:= 6 ; Option =:= 7) ->
+        command(Option,IName);
+        write("Sorry "), write(IName),
+        write("! Invalid command. Start from beginning.")
+    ),
     nl.
 
 %Displays menu to user
 main_menu:-
     write("What is your name? "),nl,
-    read_name(Name),
+    read_string(Name),
     format('Hi ~s',[Name]),
     write(". I am a chatbot. "),
     write("I can help you regarding follwoing topics - "),nl,
     general_options(),
     read(Option),
-    command(Option,Name),
-    nl.
+    ((Option =:= 1 ; Option =:= 2 ; Option =:= 3 ; Option =:= 4 ; Option =:= 5 ; Option =:= 6 ; Option =:= 7) ->
+        command(Option,Name);
+        write("Sorry "), write(Name),
+        write("! Invalid command. Start from beginning.")
+    ),
+    nl,
+    save.
---
+++ bitsdb.pl
@@ -0,0 +1,77 @@
+:- dynamic bits_payment/2.
+
+bits_payment(bits2020114241, 98908250).
+bits_payment(bits2020116539, 91929725).
+bits_payment(bits2020112125, 92410075).
+bits_payment(bits2020115762, 95458744).
+bits_payment(bits2020118682, 95618097).
+bits_payment(bits2020118888, 95288674).
+bits_payment(bits2020116205, 92207737).
+bits_payment(bits2020114353, 94350716).
+bits_payment(bits2020116136, 98454857).
+bits_payment(bits2020110246, 99659944).
+bits_payment(bits2020111797, 94205604).
+bits_payment(bits2020113449, 92933366).
+bits_payment(bits2020110604, 98058720).
+bits_payment(bits2020117415, 94790179).
+bits_payment(bits2020111063, 91517306).
+bits_payment(bits2020119972, 99222611).
+bits_payment(bits2020110045, 99040176).
+bits_payment(bits2020112790, 94000986).
+bits_payment(bits2020111278, 96059801).
+bits_payment(bits2020114358, 97827369).
+bits_payment(bits2020113666, 94214512).
+bits_payment(bits2020114911, 98097929).
+bits_payment(bits2020117564, 97046476).
+bits_payment(bits2020114320, 94750941).
+bits_payment(bits2020111736, 99418268).
+bits_payment(bits2020116017, 99060176).
+bits_payment(bits2020119480, 91285893).
+bits_payment(bits2020114107, 91653754).
+bits_payment(bits2020110171, 95293874).
+bits_payment(bits2020118950, 97340101).
+bits_payment(bits2020115535, 93455676).
+bits_payment(bits2020116762, 98625406).
+bits_payment(bits2020117817, 98335370).
+bits_payment(bits2020117810, 98335570).
+bits_payment(bits2020115011, 98099729).
+bits_payment(bits2020112191, 95288874).
+
+:- dynamic bits_score/2.
+
+bits_score(bits2020114241, 87).
+bits_score(bits2020116539, 88).
+bits_score(bits2020118682, 82).
+bits_score(bits2020118888, 86).
+bits_score(bits2020116205, 97).
+bits_score(bits2020114353, 98).
+bits_score(bits2020116136, 81).
+bits_score(bits2020110246, 88).
+bits_score(bits2020111797, 100).
+bits_score(bits2020113449, 92).
+bits_score(bits2020110604, 86).
+bits_score(bits2020117415, 82).
+bits_score(bits2020111063, 86).
+bits_score(bits2020119972, 87).
+bits_score(bits2020110045, 90).
+bits_score(bits2020112790, 82).
+bits_score(bits2020111278, 85).
+bits_score(bits2020114358, 87).
+bits_score(bits2020113666, 94).
+bits_score(bits2020114911, 86).
+bits_score(bits2020117564, 85).
+bits_score(bits2020114320, 86).
+bits_score(bits2020111736, 100).
+bits_score(bits2020116017, 97).
+bits_score(bits2020119480, 97).
+bits_score(bits2020114107, 85).
+bits_score(bits2020110171, 99).
+bits_score(bits2020118950, 92).
+bits_score(bits2020115535, 91).
+bits_score(bits2020116762, 93).
+bits_score(bits2020117817, 82).
+bits_score(bits2020117810, 81).
+bits_score(bits2020115011, 100).
+bits_score(bits2020110191, 93).
+bits_score(bits2020112191, 95).
+
