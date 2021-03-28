4e58a5736c8b1806febf6d46bccd243db016b677
droygit <59766540+droygit@users.noreply.github.com>
2021-03-29 01:36:38 +0530

    TCP echo message Server/Client
    Add a few more header files, so that we can avoid some unnecessary warnings.

--- network_programming/tcpechosrvclnt.c
+++ network_programming/tcpechosrvclnt.c
@@ -6,12 +6,14 @@
 //      2. Message length will be maximum 1500 bytes.
 
 #include <netdb.h>
+#include <unistd.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <arpa/inet.h>
 
 #define DEBUG_PRINT ;//printf("\n%s:%d", __func__, __LINE__)
 
