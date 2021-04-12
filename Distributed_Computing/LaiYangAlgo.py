7d930b377103deaaed390c0de613817706614d72
droygit <59766540+droygit@users.noreply.github.com>
2021-04-13 03:29:30 +0530

    Implementation of Lai-Yang algorithm
    Distributed computing assignment - Lai-Yang algorithm
     Few bug fixes and final output.

--- Distributed_Computing/LaiYangAlgo.py
+++ Distributed_Computing/LaiYangAlgo.py
@@ -32,10 +32,18 @@ kPosSnapshot = 8
 #kPosChannelInfo = 8
 #kPosTransitMsg = 9
 
+kHistoryPosPid = 2
+kHistoryPosTransaction = 3
+
 kTotalNumProcess = 2
+kArraySize = kTotalNumProcess + 1
 process_id = 0
 
-snapshot = []
+
+# For easy calculation, 0th column and row are ignored.
+local_snapshot = [0]*kArraySize
+send_transit_msg = [[0]*kArraySize]*kArraySize
+recv_transit_msg = [[0]*kArraySize]*kArraySize
 
 column_name = ['Event_Decription', 'Timestamp', 'Pricess_id', 'Process_Id', 'Transaction', 'Message_id']
 all_process_events = [
@@ -79,7 +87,49 @@ def print_input():
         print ('\n\n++++ Process-{0} Events ++++'.format(process[0][kPosFromPricessId]))
         df = pandas.DataFrame(process, columns=column_name)
         print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
+
+def append_global_snapshot_structure(process_id, snapshot, timestamp, event):
+#    snapshot = []
+#    snapshot.append(process_id)
+#    snapshot.append(timestamp)
+#    local_val = event[kPosTransactionAmount]
+#    snapshot.append(local_val)
+#    transit = []
+    history = event[kPosWhiteMsgHistory]
+    #print('Debug! Event = ', event)
+    for h in history:
+        if h[0] == 'S':
+            send_transit_msg[process_id][int(h[kHistoryPosPid])] = send_transit_msg[process_id][int(h[kHistoryPosPid])] + int(h[kHistoryPosTransaction])
+        elif h[0] == 'R':
+            recv_transit_msg[int(h[kHistoryPosPid])][process_id] = recv_transit_msg[int(h[kHistoryPosPid])][process_id] + int(h[kHistoryPosTransaction])
+        else:
+            print('Error! Unexpected message history = ', h)
+            
+    print('Debug! Local snapshot at Timestamp {0} Process{1} -'.format(timestamp, process_id))
+    local_snapshot[process_id] = int(snapshot)
+    print('\tDebug! Local snapshot = ', local_snapshot[process_id])
+    for i in range (1, kTotalNumProcess+1):
+        if i != process_id:
+            if send_transit_msg[process_id][i] != 0:
+                print('\tDebug! Send Channel[{0}][{1}] = {2}'.format(process_id, i, send_transit_msg[process_id][i]))
+            if recv_transit_msg[i][process_id] != 0:
+                print('\tDebug! Recv Channel[{0}][{1}] = {2}'.format(i, process_id, recv_transit_msg[i][process_id]))
     
+    #total = int(snapshot) + send_transit_msg[process_id][i] + recv_transit_msg[i][process_id]
+    #print('\tTotal = ', total)
+
+def calculate_global_snapshot():
+    print('\n\nFinal calculate of Global snapshot - ')
+    print('Calculate Global snapshot - ')
+    for i in range (1, kTotalNumProcess+1):
+        print('\tProcess{0} : Local snapshot: {1}'.format(i, local_snapshot[i]))
+        for j in range (1, kTotalNumProcess+1):
+            if i != j:
+                if send_transit_msg[process_id][i] != 0 or recv_transit_msg[i][process_id] != 0:
+                    val = send_transit_msg[i][j] - recv_transit_msg[i][j]
+                    print('\t\tChannel{0}{1} = {2} - {3} = {4}'.format(i,j,send_transit_msg[i][j], recv_transit_msg[i][j], val))
+                    
+
 
 def processing_events(process_id):
     process_events = all_process_events[process_id-1]
@@ -90,15 +140,16 @@ def processing_events(process_id):
     for i in range(kTotalNumProcess):
         channels.append(0)
         transit.append(0)
-    
+        
+    local_snapshot = 0
     final_output = []
     isRedMsgSend = True
     keepHistory = True
     col = list(column_name)
     col.append('Process_State')
     col.append('White_Msg_History')
-    col.append('Snapshot')
-    col.append('Transit')
+    #col.append('Snapshot')
+    #col.append('Transit')
     
     event = []
     
@@ -108,8 +159,8 @@ def processing_events(process_id):
         event = singleEvent[:]
         event.append(current_state) # Place holder of current state. Will be updated with appropriate value.
         event.append([])            # Place holder of White message history
-        event.append([])            # Place holder for Snapshot
-        event.append([])            # Place holder for Transit
+        #event.append([])            # Place holder for Snapshot
+        #event.append([])            # Place holder for Transit
 
         if isRedMsgSend == True:
             current_state = eProcesState.WHITE
@@ -127,6 +178,7 @@ def processing_events(process_id):
             #event[kPosChannelInfo] = channels[:]
             #event[kPosTransitMsg] = transit[:]
             keepHistory = True
+            local_snapshot = event[kPosTransactionAmount]
         elif event[kPosEventDescription] == eEvents.SEND_WHITE_MSG:
             # Message is sent before taking local snapshot
             #if current_state == eProcesState.WHITE:
@@ -142,6 +194,8 @@ def processing_events(process_id):
             isRedMsgSend = True
             keepHistory  = False
             event[kPosWhiteMsgHistory] = white_msg_history[:]
+            timestamp = event[kPosTimestamp]
+            append_global_snapshot_structure(process_id, local_snapshot, timestamp, event[:])
             #event[kPosChannelInfo] = channels[:]
             #event[kPosTransitMsg] = transit[:]
         elif event[kPosEventDescription] == eEvents.RECEIVE_WHITE_MSG:
@@ -173,6 +227,7 @@ def processing_events(process_id):
             #event[kPosChannelInfo] = channels[:]
             #event[kPosTransitMsg] = transit[:]
             keepHistory = True # Send Red message then clear the history
+            local_snapshot = event[kPosTransactionAmount]
         else:
             print('Error! Unknown event = ', event[kPosEventDescription])
         
@@ -182,30 +237,29 @@ def processing_events(process_id):
         final_output.append(list(event))
         
         
-    pandas.set_option('display.max_colwidth', 500)        
-    df = pandas.DataFrame(final_output, columns=col)
-    print('\n\nFinal Output - ')
-    print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
+    #pandas.set_option('display.max_colwidth', 500)        
+    #df = pandas.DataFrame(final_output, columns=col)
+    #print('\n\nFinal Output - ')
+    #print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
     
     
-def start_Lai_Yang(process_id):
-    processing_events(process_id)
+def start_Lai_Yang():
+    for process_id in range(1,kTotalNumProcess+1):
+        processing_events(process_id)        
+    calculate_global_snapshot()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p','--pid', help='Process id', required=True)
-    args = parser.parse_args()
-    print(args.pid)
-    process_id = int(args.pid)
-    if process_id <= 0:
-        print('Error! Process id can\'t be less or equal to zero.')
-    elif process_id != 1 and process_id != 2:
-        print('Error! Process id should be either 1 or 2.')
-    else:
-        print_input()
-        start_Lai_Yang(1)
-        start_Lai_Yang(2)
-        print_input()
-
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('-p','--pid', help='Process id', required=True)
+    #args = parser.parse_args()
+    #print(args.pid)
+    #process_id = int(args.pid)
+    #if process_id <= 0:
+    #    print('Error! Process id can\'t be less or equal to zero.')
+    #elif process_id != 1 and process_id != 2:
+    #    print('Error! Process id should be either 1 or 2.')
+    #else:
+    print_input()
+    start_Lai_Yang()
     print('---- Program Ends ----')
