88b710849d3719ed0dbba2e40137c82af72bb38c
droygit <59766540+droygit@users.noreply.github.com>
2021-04-14 02:18:25 +0530

    Implementaiton of Lai-Yang algorithm
    1. Take input from csv file
    2. Some bug fixes

--- Distributed_Computing/LaiYangAlgo.py
+++ Distributed_Computing/LaiYangAlgo.py
@@ -2,11 +2,20 @@
 # -*- coding: utf-8 -*-
 """
 @author: Debojyoti Roy
+Assumptions - 
+    1. User must run this script in a system where python 3 is installed
+    2. User has to install tabulate package in her/his machine, 
+       If not installed, please use "pip3 install tabulate" command.
+    3. This script will only work for "all_process_events" array. This array
+       has to be properly generated. Due to lack of time, I couldn't write the
+       algorithm to purse a text file and generate "all_process_events" array.
+       This task will be taken care in future.
 """
 import argparse
 from enum import Enum
 from tabulate import tabulate
 import pandas
+import csv
 
 class eProcesState(Enum):
     WHITE = 0
@@ -19,6 +28,7 @@ class eEvents(Enum):
     RECEIVE_WHITE_MSG = 3
     RECEIVE_RED_MSG = 4
     TAKE_SNAPSHOT = 5
+    UNKNOWN = 6
 
 kPosEventDescription = 0
 kPosTimestamp = 1
@@ -46,43 +56,93 @@ send_transit_msg = [[0]*kArraySize]*kArraySize
 recv_transit_msg = [[0]*kArraySize]*kArraySize
 
 column_name = ['Event_Decription', 'Timestamp', 'Pricess_id', 'Process_Id', 'Transaction', 'Message_id']
-all_process_events = [
-        #Process 1
-        [[eEvents.TIME, 1, 1, 0, 800, 'NA'],
-        [eEvents.SEND_WHITE_MSG, 1.5, 1, 2, 20, 'm12-1'],
-        [eEvents.TIME, 2, 1, 0, 780, 'NA'],
-        [eEvents.SEND_WHITE_MSG, 2.5, 1, 2, 30, 'm12-2'],
-        [eEvents.TAKE_SNAPSHOT, 3, 1, 0, 750, 'NA'],
-        [eEvents.SEND_RED_MSG, 3.5, 1, 2, 10, 'm12-3'],
-        [eEvents.TIME, 4, 1, 0, 740, 'NA'],
-        [eEvents.TIME, 5, 1, 0, 740, 'NA'],
-        [eEvents.TIME, 6, 1, 0, 740, 'NA'],
-        [eEvents.RECEIVE_WHITE_MSG, 6.5, 2, 1, 30, 'm21-1'],
-        [eEvents.TIME, 7, 1, 0, 770, 'NA'],
-        [eEvents.TIME, 8, 1, 0, 770, 'NA'],
-        [eEvents.TIME, 8, 1, 0, 770, 'NA'],
-        [eEvents.RECEIVE_WHITE_MSG, 8.5, 2, 1, 20, 'm21-2'],
-        [eEvents.TIME, 9, 1, 0, 790, 'NA']],
-        #Process 2
-        [[eEvents.TIME, 1, 2, 0, 200, 'NA'],
-        [eEvents.TIME, 2, 2, 0, 200, 'NA'],
-        [eEvents.RECEIVE_WHITE_MSG, 2.5, 1, 2, 20, 'm12-1'],
-        [eEvents.TIME, 3, 2, 0, 220, 'NA'],
-        [eEvents.TIME, 4, 2, 0, 220, 'NA'],
-        [eEvents.SEND_WHITE_MSG, 6.5, 2, 1, 30, 'm21-1'],
-        [eEvents.TIME, 5, 2, 0, 190, 'NA'],
-        [eEvents.RECEIVE_RED_MSG, 5.5, 1, 2, 10, 'm12-2'],
-        [eEvents.SEND_RED_MSG, 5.6, 2, 1, 0, 'm21-2'],
-        [eEvents.TIME, 6, 2, 0, 200, 'NA'],
-        [eEvents.RECEIVE_WHITE_MSG, 6.5, 1, 2, 30, 'm12-3'],
-        [eEvents.TIME, 7, 2, 0, 230, 'NA'],
-        [eEvents.SEND_WHITE_MSG, 8.5, 2, 1, 20, 'm21-3'],
-        [eEvents.TIME, 8, 2, 0, 210, 'NA'],
-        [eEvents.TIME, 9, 2, 0, 210, 'NA']]
-]
+#hard_coded_all_process_events = [
+#        #Process 1
+#        [[eEvents.TIME, 1, 1, 0, 800, 'NA'],
+#        [eEvents.SEND_WHITE_MSG, 1.5, 1, 2, 20, 'm12-1'],
+#        [eEvents.TIME, 2, 1, 0, 780, 'NA'],
+#        [eEvents.SEND_WHITE_MSG, 2.5, 1, 2, 30, 'm12-2'],
+#        [eEvents.TAKE_SNAPSHOT, 3, 1, 0, 750, 'NA'],
+#        [eEvents.SEND_RED_MSG, 3.5, 1, 2, 10, 'm12-3'],
+#        [eEvents.TIME, 4, 1, 0, 740, 'NA'],
+#        [eEvents.TIME, 5, 1, 0, 740, 'NA'],
+#        [eEvents.TIME, 6, 1, 0, 740, 'NA'],
+#        [eEvents.RECEIVE_WHITE_MSG, 6.5, 2, 1, 30, 'm21-1'],
+#        [eEvents.TIME, 7, 1, 0, 770, 'NA'],
+#        [eEvents.TIME, 8, 1, 0, 770, 'NA'],
+#        [eEvents.TIME, 8, 1, 0, 770, 'NA'],
+#        [eEvents.RECEIVE_WHITE_MSG, 8.5, 2, 1, 20, 'm21-2'],
+#        [eEvents.TIME, 9, 1, 0, 790, 'NA']],
+#        #Process 2
+#        [[eEvents.TIME, 1, 2, 0, 200, 'NA'],
+#        [eEvents.TIME, 2, 2, 0, 200, 'NA'],
+#        [eEvents.RECEIVE_WHITE_MSG, 2.5, 1, 2, 20, 'm12-1'],
+#        [eEvents.TIME, 3, 2, 0, 220, 'NA'],
+#        [eEvents.TIME, 4, 2, 0, 220, 'NA'],
+#        [eEvents.SEND_WHITE_MSG, 6.5, 2, 1, 30, 'm21-1'],
+#        [eEvents.TIME, 5, 2, 0, 190, 'NA'],
+#        [eEvents.RECEIVE_RED_MSG, 5.5, 1, 2, 10, 'm12-2'],
+#        [eEvents.SEND_RED_MSG, 5.6, 2, 1, 0, 'm21-2'],
+#        [eEvents.TIME, 6, 2, 0, 200, 'NA'],
+#        [eEvents.RECEIVE_WHITE_MSG, 6.5, 1, 2, 30, 'm12-3'],
+#        [eEvents.TIME, 7, 2, 0, 230, 'NA'],
+#        [eEvents.SEND_WHITE_MSG, 8.5, 2, 1, 20, 'm21-3'],
+#        [eEvents.TIME, 8, 2, 0, 210, 'NA'],
+#        [eEvents.TIME, 9, 2, 0, 210, 'NA']]
+#]
+
+#all_process_events = hard_coded_all_process_events
+all_process_events = list()
+
+def convertEventStrToEnum(event_string):
+    ret_enum = eEvents.UNKNOWN
+    if event_string == 'TIME':
+        ret_enum = eEvents.TIME
+    elif event_string == 'SEND_WHITE_MSG':
+        ret_enum = eEvents.SEND_WHITE_MSG
+    elif event_string == 'SEND_RED_MSG':
+        ret_enum = eEvents.SEND_RED_MSG
+    elif event_string == 'RECEIVE_WHITE_MSG':
+        ret_enum = eEvents.RECEIVE_WHITE_MSG
+    elif event_string == 'RECEIVE_RED_MSG':
+        ret_enum = eEvents.RECEIVE_RED_MSG
+    elif event_string == 'TAKE_SNAPSHOT':
+        ret_enum = eEvents.TAKE_SNAPSHOT
+    else:
+        print('Error! Unknown event string = ', event_string)
+        
+    return ret_enum
+    
+        
+
+def read_input(totalNumProcess):
+
+    #temp_all_process_events = list()
+    for i in range (totalNumProcess):
+        #temp_all_process_events.append(list())
+        all_process_events.append(list())
+        
+    #print (temp_all_process_events)
+    with open('input.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            pid = int(row[0]) - 1
+            row[1] = convertEventStrToEnum(row[1])
+            all_process_events[pid].append(list (row[1:7]))
+            
+    #all_process_events = temp_all_process_events[:]
+    
+#    print (temp_all_process_events)
+#    print ('\n\nPursing csv and building input array - ')
+#    for process in temp_all_process_events:
+#        print ('\n\n++++ Process-{0} Events ++++'.format(int(process[0][kPosFromPricessId])))
+#        #print(process)
+#        df = pandas.DataFrame(process, columns=column_name)
+#        print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
+    
+    
 
 def print_input():
-    #TODO: Read from file and create 'all_process_events' array
     for process in all_process_events:
         print ('\n\n++++ Process-{0} Events ++++'.format(process[0][kPosFromPricessId]))
         df = pandas.DataFrame(process, columns=column_name)
@@ -148,8 +208,6 @@ def processing_events(process_id):
     col = list(column_name)
     col.append('Process_State')
     col.append('White_Msg_History')
-    #col.append('Snapshot')
-    #col.append('Transit')
     
     event = []
     
@@ -159,8 +217,6 @@ def processing_events(process_id):
         event = singleEvent[:]
         event.append(current_state) # Place holder of current state. Will be updated with appropriate value.
         event.append([])            # Place holder of White message history
-        #event.append([])            # Place holder for Snapshot
-        #event.append([])            # Place holder for Transit
 
         if isRedMsgSend == True:
             current_state = eProcesState.WHITE
@@ -181,13 +237,8 @@ def processing_events(process_id):
             local_snapshot = event[kPosTransactionAmount]
         elif event[kPosEventDescription] == eEvents.SEND_WHITE_MSG:
             # Message is sent before taking local snapshot
-            #if current_state == eProcesState.WHITE:
             msg = ['S', event[kPosMessageId], event[kPosToPricessId], event[kPosTransactionAmount]]
             white_msg_history.append(msg)
-                #print('Debug! white_msg_history = ', white_msg_history)
-            #event[kPosWhiteMsgHistory] = white_msg_history[:]
-            #event[kPosChannelInfo] = channels[:]
-            #event[kPosTransitMsg] = transit[:]
             keepHistory = True
         elif event[kPosEventDescription] == eEvents.SEND_RED_MSG:
             # Message is sent after taking local snapshot
@@ -196,17 +247,11 @@ def processing_events(process_id):
             event[kPosWhiteMsgHistory] = white_msg_history[:]
             timestamp = event[kPosTimestamp]
             append_global_snapshot_structure(process_id, local_snapshot, timestamp, event[:])
-            #event[kPosChannelInfo] = channels[:]
-            #event[kPosTransitMsg] = transit[:]
         elif event[kPosEventDescription] == eEvents.RECEIVE_WHITE_MSG:
             msg = ['R', event[kPosMessageId], event[kPosFromPricessId], event[kPosTransactionAmount]]
             white_msg_history.append(msg)
-            #print('Debug! white_msg_history = ', white_msg_history)
-            #event[kPosWhiteMsgHistory] = white_msg_history[:]
             if current_state == eProcesState.RED:
                 event[kPosWhiteMsgHistory] = white_msg_history [:]
-            #event[kPosChannelInfo] = channels[:]
-            #event[kPosTransitMsg] = transit[:]
             keepHistory = True
         elif event[kPosEventDescription] == eEvents.RECEIVE_RED_MSG:
             # This process should take its local snapshot
@@ -214,8 +259,6 @@ def processing_events(process_id):
             # it records its local snapshot before processing the message
             current_state = eProcesState.RED
             event[kPosWhiteMsgHistory] = white_msg_history[:]
-            #event[kPosChannelInfo] = channels[:]
-            #event[kPosTransitMsg] = transit[:]
             keepHistory = True # TODO: Should I clear ?
             isRedMsgSend = False
         elif event[kPosEventDescription] == eEvents.TAKE_SNAPSHOT:
@@ -223,16 +266,12 @@ def processing_events(process_id):
             # At this time, this process turned red
             current_state = eProcesState.RED
             isRedMsgSend = False
-            #event[kPosWhiteMsgHistory] = white_msg_history[:]
-            #event[kPosChannelInfo] = channels[:]
-            #event[kPosTransitMsg] = transit[:]
             keepHistory = True # Send Red message then clear the history
             local_snapshot = event[kPosTransactionAmount]
         else:
             print('Error! Unknown event = ', event[kPosEventDescription])
         
         event[kPosProcessState] = current_state
-        #print('Debug! Append event = ', event)
         
         final_output.append(list(event))
         
@@ -250,16 +289,14 @@ def start_Lai_Yang():
 
 
 if __name__ == "__main__":
-    #parser = argparse.ArgumentParser()
-    #parser.add_argument('-p','--pid', help='Process id', required=True)
-    #args = parser.parse_args()
-    #print(args.pid)
-    #process_id = int(args.pid)
-    #if process_id <= 0:
-    #    print('Error! Process id can\'t be less or equal to zero.')
-    #elif process_id != 1 and process_id != 2:
-    #    print('Error! Process id should be either 1 or 2.')
-    #else:
-    print_input()
-    start_Lai_Yang()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p','--npid', help='Number of Processes participates in this calculation', required=True)
+    args = parser.parse_args()
+    kTotalNumProcess = int(args.npid)
+    if kTotalNumProcess <= 1:
+        print('Error! Number of process can\'t be less or equal to 1. It should be at least 2.')
+    else:
+        read_input(kTotalNumProcess)
+        print_input()
+        start_Lai_Yang()
     print('---- Program Ends ----')
